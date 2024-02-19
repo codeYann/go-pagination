224f54fae969e2fd03ac8b37bd0c9bdd034571f4
Yan Avlis <ccyan05@alu.ufc.br>
2024-02-19 11:16:34 -0300

    Entry point.

--- cmd/go-pagination/main.go
+++ cmd/go-pagination/main.go
@@ -2,25 +2,31 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"runtime"
+	"time"
 
 	"github.com/codeYann/go-pagination/internal/requests"
 )
 
 func main() {
-	runtime.GOMAXPROCS(1)
+	runtime.GOMAXPROCS(2)
 
 	request := requests.HTTPRequest{
-		BaseURL: "https://jsonplaceholder.typicode.com/",
-		Method:  "GET",
+		BaseURL:    "https://www.mercadobitcoin.net/api/BTC/",
+		Method:     "GET",
+		MaxTimeout: 300 * time.Millisecond,
 	}
 
-	url := "posts"
+	url := "trades"
+	ctx := context.Background()
 
-	data, err := request.Request(url)
+	data, err := request.MakeRequest(ctx, url)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
+		return
 	}
 
 	fmt.Println(string(data))
